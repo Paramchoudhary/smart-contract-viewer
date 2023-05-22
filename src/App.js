a04a8aead79d4fae0e6039f45990d43c7a7e3e3a
Param Choudhary <choudharyparam99@gmail.com>
2023-05-22 23:19:03 +0530

    Initial commit of all files to the repository

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContractViewer from './ContractViewer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Smart Contract Viewer</h1>
+      <ContractViewer />
+    </div>
+  );
+}
+
+export default App;
